bb91708909308d5252c4cf7cc702f58a76598d10
Gonzalo Diamend <gonzalo.diamend@gmail.com>
2020-07-03 11:16:03 -0300

    Adding dependencies

---
+++ Tests/index.js
@@ -0,0 +1,11 @@
+const {Builder, By, until} = require('selenium-webdriver');
+
+let driver = new Builder()
+    .forBrowser('chrome')
+    .build();
+
+driver.get('http://www.google.com/ncr');
+driver.findElement(By.name('q')).sendKeys('webdriver');
+driver.findElement(By.name('btnK')).click('enter');
+driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+driver.quit();
